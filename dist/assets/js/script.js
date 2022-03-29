395dd6401c5abbf8ed40057bf48b975f7c77af53
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-03-29 23:08:23 +0300

    add section clients markup

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,4 +1,6 @@
 
+//Инициализация Swiper в секции tariff
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -14,4 +16,20 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+//Инициализация Swiper в секции clients
+
+const swiper2 = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  effect: 'slide',
+  speed: 300,
+  autoplay: true,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+});
 //# sourceMappingURL=script.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,23 @@
+//Инициализация Swiper в секции tariff
+
 const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  effect: 'slide',
+  speed: 300,
+  autoplay: true,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+});
+
+//Инициализация Swiper в секции clients
+
+const swiper2 = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   effect: 'slide',
